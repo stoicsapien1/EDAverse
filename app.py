0b8d6cd9826630bc9030bee78fd20865e347905e
Belal Ahmed Siddiqui <belalahmedsiiddiqui8@gmail.com>
2024-06-14 10:18:36 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+from st_aggrid import AgGrid
+import ydata_profiling
+from ydata_profiling import ProfileReport
+from PIL import Image
+
+st.set_page_config(layout="wide")
+st.sidebar.title("Welcome to EDAverse")
+with st.sidebar.expander("About the App💻"):
+    st.write("""EDAverse is made using Streamlit and pandas_profiling package.You can use the app to quickly generate a comprehensive data profiling and EDA report using even writing a single line code.📊📊
+    """)
+st.title("EDAverse📊💹")
+st.write("Explore. Discover. Analyze.")
